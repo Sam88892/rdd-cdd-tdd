365a6d27af9fca73d8312c9703195c887abcc490
Sam88892 <ditchi.samuel@gmail.com>
2017-02-24 19:05:07 +0100

    Update index.js
    I comment the code

--- weather/index.js
+++ weather/index.js
@@ -1,20 +1,20 @@
-'use strict';
+'use strict'; //Choose a restrictive variant of JavaScript
 
-const YQL = require('yql');
+const YQL = require('yql');  // Call the requierements
 const _ = require('lodash');
 
 module.exports = (opts, callback) => {
-	opts = opts || [];
+	opts = opts || []; //Creat an array with options
 
 	let query;
 
-	if (_.isEmpty(opts)) {
+	if (_.isEmpty(opts)) { //database management
 		query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="Dhaka, Bangladesh")');
 	} else {
 		query = new YQL('select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="' + opts[0] + ', ' + opts[1] + '")');
 	}
 
-	query.exec((err, response) => {
+	query.exec((err, response) => { //error management
 		if (err) {
 			return callback(err);
 		}
